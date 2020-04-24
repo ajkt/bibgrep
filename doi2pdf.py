883e6b0cec159a6c9fc1f743dc0e4397b179a874
Kate Rogers <katerogers@vimes.speedport.ip>
2020-04-24 18:42:24 +0200

    added count output and a check to not re-download existing files

--- doi2pdf.py
+++ doi2pdf.py
@@ -6,13 +6,13 @@ import os
 
 
 def doi2pdf(doi):
-	print("trying to download",doi)
+	print("... for",doi)
 	
 	prefix = "https://dl.acm.org/doi/pdf/"
 	suffix = "?download=true"
 
 	url = prefix + doi + suffix
-	print("trying URL:", url)
+	print("... trying URL:", url)
 
 	r = requests.get(url, stream=True)
 	with open("pdfs/%s.pdf" % doi.replace("/","--"), 'wb') as f:
@@ -31,15 +31,24 @@ def scrapeFromDOIs(doilist):
 	count = 1
 	for doi in doilist:
 		
+		# file already exists
+		if (os.path.isfile("pdfs/" + doi.replace("/", "--") + ".pdf")):
+			continue	
+
+		# file doesn't exist yet
 		print("... downloading", doi)
 		doi2pdf(doi)
 		count += 1
 		
 		delay = random.randrange(15,60)
-		print("... waiting", delay, "seconds")	
+		print("... that was" + count + "/" + len(doilist) + ". now waiting", delay, "seconds")	
 		time.sleep(delay)
 
-	print("...done downloading", count, "pdfs")	
+	count -= 1
+	if (count == 0):
+		print("... all bib files already downloaded")
+	else:
+		print("...done downloading", count, "pdfs")	
 
 	return
 
